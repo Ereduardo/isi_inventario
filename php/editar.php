f5ef8667e352754d24dbea0b596369de51e0294e
Ereduardo <eduajimenez95@gmail.com>
2022-11-07 16:43:11 -0500

    Subiendo proyecto a github

---
+++ php/editar.php
@@ -0,0 +1,211 @@
+<?php
+session_start();
+require_once "../Database/Database.php";
+if ($_SESSION['username'] == null) {
+    echo "<script>alert('Please login.');</script>";
+    header("Refresh:0 , url= ../index.html");
+    exit();
+}
+$username = $_SESSION['username'];
+$sql_fetch_todos = "SELECT * FROM product ORDER BY id ASC";
+$query = mysqli_query($conn, $sql_fetch_todos);
+
+?>
+<!doctype html>
+<html lang="en">
+
+<head>
+    <title>Lista de Productos</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="icon" href=" ../imagen.png">
+    <link rel="stylesheet" href="../styles.css" type="text/css" />
+    <style>
+        body {
+            font-family: Arial, Helvetica, sans-serif;
+            margin: 0;
+            background: url(../imagenes/imagen.jpg);
+            background-size: cover;
+            background-attachment: fixed;
+        }
+        .header {
+            position: fixed;
+            top: 0px;
+            left: 0px;
+            right: 0px;
+            height: 50px;
+            padding: 5px 13px 11px 0px;
+            width: 100%;
+            color: white;
+            font-family: Arial, Helvetica, sans-serif;
+            margin-top: 0px;
+            bottom: 0;
+            background-color: #298dba;
+        }
+        .header p {
+            margin-left: 20px;
+            text-align: left;
+        }
+        .button-logout {
+            position: relative;
+            margin-top: -50px;
+            margin-right: 20px;
+            float: right;
+            text-decoration: none;
+            border: transparent;
+            border-radius: 15px;
+            background-color: #e60000;
+            padding: 10px 10px 10px 10px;
+            color: white;
+            transition: 0.5s;
+        }
+        .button-logout:hover {
+            background-color: #D9ddd4;
+            color: red;
+        }
+        table th,
+        tr,
+        td {
+            border: 1px solid black;
+            border-collapse: collapse;
+            padding: 10px 0px 10px 0px;
+        }
+        table {
+            width: 100%;
+        }
+        th {
+            color: white;
+            background-color: #298dba;
+        }
+        tr {
+            background-color: white;
+        }
+        tr:nth-child(even) {
+            background-color: #f2f2f2;
+        }
+        .timeregis {
+            text-align: center;
+        }
+        .modify {
+            text-align: center;
+        }
+        .delete {
+            text-align: center;
+        }
+        .modify .bfix {
+            border-radius: 15px;
+            background-color: #ffcc33;
+            color: black;
+            text-decoration: none;
+            padding: 4px 20px 4px 20px;
+            transition: 0.5s;
+        }
+        .modify .bfix:hover {
+            background-color: #fdb515;
+            color: white;
+        }
+        .delete .bdelete {
+            border-radius: 15px;
+            background-color: #e60000;
+            text-decoration: none;
+            color: white;
+            padding: 4px 20px 4px 20px;
+            transition: 0.5s;
+        }
+        .delete .bdelete:hover {
+            background-color: #D9ddd4;
+            color: red;
+        }
+        .Addlist {
+            margin-right: 100px;
+            padding: 5px 30px 5px 30px;
+            border-radius: 15px;
+            text-decoration: none;
+            color: white;
+            background-color: #00A600;
+            transition: 0.5s;
+        }
+        .Addlist:hover {
+            color: black;
+            background-color: #BBFFBB;
+        }
+
+        .return {
+            border-radius: 15px;
+            background-color: #ffcc33;
+            color: black;
+            text-decoration: none;
+            padding: 4px 40px 4px 40px;
+            margin: 0px 0px 50px 100px;
+            font-size: 20px;
+            transition: 0.5s;
+
+        }
+
+        .return:hover {
+            background-color: #fdb515;
+            color: white;
+        }
+    </style>
+</head>
+<body>
+    <div class="header">
+        <h1>TU INVENTARIO</h1>
+        <br>
+        <a name="" id="" class="button-logout" href="logout.php" role="button">Cerrar Sesión</a>
+    </div>
+    <br>
+    <br>
+    <br>
+    <div id="dolphincontainer">
+      <div id="dolphinnav">
+        <ul>
+          <li><a href="../inicio.html"><span>INICIO</span></a></li>
+          <li><a href="list.php" class="current"><span>PRODUCTOS</span></a></li>
+        </ul>
+      </div>
+    </div>
+    <div>
+        <h2 style="color:#FFFFFF">Lista de Productos</h2>
+    </div>
+    <div class="table-product">
+        <table>
+            <tr>
+                <th scope="col">Orden</th>
+                <th scope="col">ID:Producto</th>
+                <th scope="col">Nombre:Producto</th>
+                <th scope="col">Cantidades</th>
+                <th scope="col">Fecha:Registro</th>
+                <th scope="col">Editar</th>
+            </tr>
+            <tbody>
+                <?php
+                $idpro = 1;
+                while ($row = mysqli_fetch_array($query)) { ?>
+                    <tr>
+                        <td scope="row"><?php echo $idpro ?></td>
+                        <td><?php echo $row['id'] ?></td>
+                        <td><?php echo $row['proname'] ?></td>
+                        <td><?php echo $row['amount'] ?></td>
+                        <td class="timeregis"><?php echo $row['time'] ?></td>
+                        <td class="modify"><a name="edit" id="" class="bfix" href="fix.php?id=<?php echo $row['id'] ?>&message=<?php echo $row['proname'] ?>&amount=<?php echo $row['amount']; ?> " role="button">
+                                Editar
+                            </a></td>
+                    </tr>
+                <?php
+                    $idpro++;
+                } ?>
+            </tbody>
+            
+        </table>
+    </div>
+    <div class="form-button">
+                    <a name="" id="" class="return" href="list.php" role="button" style="float:left">Volver</a>
+            </div>
+    <?php
+    mysqli_close($conn);
+    ?>
+</body>
+
+</html>
